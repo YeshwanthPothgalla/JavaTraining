71d73386c001ba70b8e1803ae9852a9162b87f25
YeshwanthPothagalla <101534006+YeshwanthPothagalla@users.noreply.github.com>
2023-06-13 17:07:17 -0500

    using nested loops and not conditions

---
+++ src/CaseInsensitivity.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CaseInsensitivity {
+
+    public static void main(String[] args) {
+        final String START = "start";
+        final String STOP = "stop";
+        final String QUIT = "quit";
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter the Command:");
+            String command = sc.nextLine();
+            if (!command.isBlank()) {
+                if (command.equalsIgnoreCase(START)) {
+                    System.out.println("Machine Starting...");
+                } else if (command.equalsIgnoreCase(STOP)) {
+                    System.out.println("Machine Stopping...");
+                } else if (command.equalsIgnoreCase(QUIT)) {
+                    System.out.println("Exiting...");
+                    break;
+                } else {
+                    System.out.println("Invalid Command");
+                }
+            }
+        }
+
+    }
+}
